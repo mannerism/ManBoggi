1016a37aab5be20e47934db34c660b3ddf0c435f
Juno Yu <jxy35@hitit.xyz>
2020-11-18 23:32:11 +0900

    code only changed

---
+++ ManBoGi/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  ManBoGi
+//
+//  Created by Yu Juno on 2020/11/18.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+}
