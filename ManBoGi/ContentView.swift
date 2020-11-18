ef9c4017a5540e046a9c38ffbb6672c55d388b9d
Juno Yu <jxy35@hitit.xyz>
2020-11-18 23:12:34 +0900

    first commit

---
+++ ManBoGi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ManBoGi
+//
+//  Created by Yu Juno on 2020/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
